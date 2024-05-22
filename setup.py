a7162dfa17e9ed3726b9bb33da8b543e5719fab8
Ruo Yu Tao <ruoyutao@gmail.com>
2024-05-21 23:46:11 -0400

    change name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='partially-observable-rl',
+    name='lambda-discrepancy',
     version='0.1.0',
     packages=find_packages(),
 )
